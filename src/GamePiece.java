502816023963c023e3f45aad23030ade252a39cb
Evan Coakley <evancoakley1@gmail.com>
2017-09-12 15:19:31 -0500

    testing

--- src/GamePiece.java
+++ src/GamePiece.java
@@ -34,12 +34,29 @@ public class GamePiece {
         this.color = color;
     }
 
-    public GamePiece(int positionX, int positionY, boolean frozen, String name, String color) {
+    public GamePiece() {
         this.positionX = 0;
         this.positionY = 0;
         this.frozen = false;
         this.name = name;
         this.color = color;
     }
+
+    void move(int x, int y) {
+        if(!frozen) {
+            this.positionX = x;
+            this.positionY = y;
+            System.out.println("Coordinates Changed");
+        } else{
+            System.out.println("Piece doesn't move");
+        }
+    }
+    void freeze() {
+        this.frozen = true;
+    }
+    void unfreeze() {
+        this.frozen = false;
+    }
+
 
 }
--- src/Runner.java
+++ src/Runner.java
@@ -3,6 +3,14 @@ public class Runner {
 
     public static void main(String[] args) {
 
+    GamePiece gamePiece = new GamePiece();
+
+        gamePiece.setName("Bishop");
+        gamePiece.setColor("Black");
+        gamePiece.move(1, 2);
+        System.out.println(gamePiece.getPositionX());
+        System.out.println(gamePiece.getPositionY());
+
 
     }
 }
